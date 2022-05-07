57bda11146666d09d653f11843928e31790a1ee8
hhg21011998 <hhg21011998@gmail.com>
2022-05-07 08:46:07 +0700

    update ex 02-05

--- 05_iteration.py
+++ 05_iteration.py
@@ -2,22 +2,22 @@ print("hello world")
 
 # Exercise 1: Write a program which repeatedly reads numbers until the user enters “done”. Once “done” is entered, print out the total, count, and average of the numbers.
 # If the user enters anything other than a number, detect their mistake using try and except and print an error message and skip to the next number.
-# total = 0
-# count = 0
-# average = 0
-#
-# while True:
-#     userInput = input("Enter a number: ")
-#     try:
-#         if userInput == "done":
-#             break
-#         else:
-#             total += int(userInput)
-#             count += 1
-#             average = total / count
-#     except:
-#         print("Invalid input")
-# print(total, count, average)
+total = 0
+count = 0
+average = 0
+
+while True:
+    userInput = input("Enter a number: ")
+    try:
+        if userInput == "done":
+            break
+        else:
+            total += int(userInput)
+            count += 1
+            average = total / count
+    except:
+        print("Invalid input")
+print(total, count, average)
 
 # Exercise 2: Write another program that prompts for a list of numbers as above and at the end prints out both the maximum and minimum of the numbers instead of the average.
 myMax = None
@@ -26,19 +26,15 @@ myMin = None
 while True:
     userInput = input("Enter a number: ")
 
-    # try:
-    if userInput == "done":
-        break
-    else:
-        intInput = int(userInput)
-        if myMax is not None or myMax < intInput:
-            myMax = intInput
-        elif myMin is not None or myMin > intInput:
-            myMin = intInput
-        elif myMax is None:
-            myMax = intInput
-        elif myMin is None:
-            myMin = intInput
-    # except:
-    #     print("Invalid input")
-print("My min = ", myMin, "My max = ", myMax)
+    try:
+        if userInput == "done":
+            break
+        else:
+            intInput = int(userInput)
+            if myMax is None or myMax < intInput:
+                myMax = intInput
+            if myMin is None or myMin > intInput:
+                myMin = intInput
+    except:
+        print("Invalid input")
+print("My min =", myMin, "My max =", myMax)
