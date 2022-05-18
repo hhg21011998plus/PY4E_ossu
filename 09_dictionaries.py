3c31f8ed35877a644815edebe9cef0574e838a49
hhg21011998plus <hhg21011998@gmail.com>
2022-05-18 21:19:46 +0700

    add pr

--- 09_dictionaries.py
+++ 09_dictionaries.py
@@ -13,3 +13,8 @@ for line in fhand:
             dictWords[word] = 1
 
 print("and" in dictWords)
+
+# Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done. 
+# To do this look for lines that start with “From”, then look for the third word and
+# keep a running count of each of the days of the week.
+# At the end of the program print out the contents of your dictionary (order does not matter).
