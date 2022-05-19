337ec4905bb1a0bc6e84e96942157fa7d4d388c8
hhg21011998plus <hhg21011998@gmail.com>
2022-05-19 20:42:04 +0700

    added exercise 9.2

--- 09_dictionaries.py
+++ 09_dictionaries.py
@@ -3,18 +3,33 @@
 # It doesn’t matter what the values are.
 # Then you can use the in operator as a fast way to check whether a string is in the dictionary.
 
-fhand = open("words.txt")
-dictWords = dict()
+# fhand = open("words.txt")
+# dictWords = dict()
+#
+# for line in fhand:
+#     flist = line.split()
+#     if len(flist) > 0:
+#         for word in flist:
+#             dictWords[word] = 1
+#
+# print("and" in dictWords)
 
-for line in fhand:
-    flist = line.split()
-    if len(flist) > 0:
-        for word in flist:
-            dictWords[word] = 1
-
-print("and" in dictWords)
-
-# Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done. 
+# Exercise 2: Write a program that categorizes each mail message by which day of the week the commit was done.
 # To do this look for lines that start with “From”, then look for the third word and
 # keep a running count of each of the days of the week.
 # At the end of the program print out the contents of your dictionary (order does not matter).
+fname = input("Enter a file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("Error file name %s" %fname)
+
+counts = dict()
+
+for line in fhand:
+    line = line.strip()
+    lineList = line.split()
+    if len(lineList) > 0 and lineList[0] == "From":
+        counts[lineList[2]] = counts.get(lineList[2], 0) + 1
+
+print(counts)
