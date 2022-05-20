7a8006cd3065b84e08f93468f329370f3e2fce38
hhg21011998plus <hhg21011998@gmail.com>
2022-05-20 22:01:26 +0700

    done exercise 2

--- 10_tuples.py
+++ 10_tuples.py
@@ -3,6 +3,38 @@
 # After all the data has been read, print the person with the most commits by creating a list of (count, email) tuples from the dictionary.
 # Then sort the list in reverse order and print out the person who has the most commits.
 
+# fname = input("Enter a file name: ")
+#
+# try:
+#     fhand = open(fname)
+# except:
+#     print("Error file name!")
+#     exit()
+#
+# addressDict = dict()
+#
+# for line in fhand:
+#     line = line.rstrip()
+#     line = line.split()
+#     if len(line) > 0 and line[0] == "From":
+#         address = line[1]
+#         addressDict[address] = addressDict.get(address, 0) + 1
+#
+# addressList = list()
+#
+# for key in addressDict:
+#     addressList.append((addressDict[key], key))
+#
+# addressList.sort(reverse = True)
+# a, b = addressList[0]
+#
+# print(b, a)
+
+# Exercise 2: This program counts the distribution of the hour of the day for each of the messages.
+# You can pull the hour from the “From” line by finding the time string and
+# then splitting that string into parts using the colon character.
+# Once you have accumulated the counts for each hour, print out the counts, one per line, sorted by hour as shown below.
+
 fname = input("Enter a file name: ")
 
 try:
@@ -11,21 +43,23 @@ except:
     print("Error file name!")
     exit()
 
-addressDict = dict()
+hourDict = dict()
 
 for line in fhand:
     line = line.rstrip()
     line = line.split()
     if len(line) > 0 and line[0] == "From":
-        address = line[1]
-        addressDict[address] = addressDict.get(address, 0) + 1
+        time = line[5]
+        time = time.split(":")
+        hour = time[0]
+        hourDict[hour] = hourDict.get(hour, 0) + 1
 
-addressList = list()
+hourList = list()
 
-for key in addressDict:
-    addressList.append((addressDict[key], key))
+for key in hourDict:
+    hourList.append((key, hourDict[key]))
 
-addressList.sort(reverse = True)
-a, b = addressList[0]
+hourList.sort()
 
-print(b, a)
+for a, b in hourList:
+    print(a, b)
