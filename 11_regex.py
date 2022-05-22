0ff69593259c0c7dbc9f73c7b8d952898af21cc1
hhg21011998plus <hhg21011998@gmail.com>
2022-05-22 10:43:54 +0700

    done exercise 11.1

--- 11_regex.py
+++ 11_regex.py
@@ -1,1 +1,14 @@
-print("hello chap 11")
+import re
+
+# Exercise 1: Write a simple program to simulate the operation of the grep command on Unix.
+# Ask the user to enter a regular expression and count the number of lines that matched the regular expression:
+
+typing = input("Enter a regex: ")
+fhand = open("mbox.txt")
+count = 0
+
+for line in fhand:
+    if re.search("%s" %typing, line):
+        count += 1
+
+print("mbox.txt had %d lines that matched %s" %(count, typing))
